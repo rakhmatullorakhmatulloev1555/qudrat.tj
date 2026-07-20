a243453b33cb8ecd1a9f69eb0bcbc4830de4c33d
Rakhmatullo <65566629+rakhmatullorakhmatulloev1555@users.noreply.github.com>
2026-07-21 01:14:06 +0500

    Create English navigation language file
    Add navigation language file with English translations.

---
+++ lang/en/nav.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home'      => 'Home',
+    'objects'   => 'Properties',
+    'investors' => 'Investors',
+    'services'  => 'Services',
+    'about'     => 'About Us',
+    'contacts'  => 'Contacts',
+    'consult'   => 'Consultation',
+];
